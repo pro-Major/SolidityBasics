3cbacce4fb40b549caad344211894e436f3b64ae
Major <navikbipin@gmail.com>
2022-04-03 10:18:00 +0530

    Global Variable.

---
+++ golbalVariable.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.9.0;
+contract demo 
+{
+    function getter() view public returns(uint block_no,uint timestamp,address msgSender){
+        return(block.number,block.timestamp,msg.sender);
+    }
+}
